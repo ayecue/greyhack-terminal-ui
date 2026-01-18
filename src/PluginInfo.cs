e94a407ffed491d99cedee9701d5ba7a2ff658e5
ayecue <soerenwehmeier@googlemail.com>
2026-01-18 19:02:39 +0100

    0.2.0 bump version

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace GreyHackTerminalUI
   {
     public const string PLUGIN_GUID = "GreyHackTerminalUI";
     public const string PLUGIN_NAME = "GreyHack Terminal UI";
-    public const string PLUGIN_VERSION = "0.1.0";
+    public const string PLUGIN_VERSION = "0.2.0";
   }
 }
